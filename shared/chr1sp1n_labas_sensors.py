0c37aac9cef1d5da47aeef05235025ea064d3fad
Chris Spin <chr1sp1n.dev@gmail.com>
2018-08-16 02:07:36 +0200

    path module

--- shared/chr1sp1n_labas_sensors.py
+++ shared/chr1sp1n_labas_sensors.py
@@ -1,5 +1,4 @@
-
-
+from pathlib import PurePath, Path
 
 class North():
     """Read heading position related to north"""
@@ -22,19 +21,34 @@ class North():
 class Incl():
     """Read incl sensors"""
 
-    def read(self):
-        x = self.read_file('/sys/bus/iio/devices/iio:device7/in_incli_x_raw')
-        y = self.read_file('/sys/bus/iio/devices/iio:device7/in_incli_y_raw')
-        z = self.read_file('/sys/bus/iio/devices/iio:device7/in_incli_z_raw')
-        return [ self.get_incl_value(x), self.get_incl_value(y), self.get_incl_value(z) ]
+    def __init__(self):
+        try:
+            self.file_path = None
+            for d in range(1, 9):
+                dir_name = Path('/sys/bus/iio/devices/iio:device' + str(d))
+                if dir_name.is_dir():
+                    for f in "xyz":
+                        file_name = Path( str(dir_name) + '/in_incli_' + f +'_raw' )
+                        if file_name.is_file():
+                            dir_ok = True
+                        else:
+                            dir_ok = False
+                            break
+                    if dir_ok:
+                        self.file_path = dir_name
+                        break
+            if self.file_path == None:
+                raise ValueError("[ERRO] Can't find sensor file.")
+        except:
+            print ("[ERRO] Can't find sensor file.")
+            exit(1)
+        
 
-    def read_file(self, file_name):
-        f = open(file_name, 'r')
-        v = f.readline()
-        f.close()
-        v = str(v)
-        v = v[0:-1]
-        return v
+    def read(self):
+        x = Path(self.file_path, 'in_incli_x_raw').read_text()
+        y = Path(self.file_path, 'in_incli_y_raw').read_text()
+        z = Path(self.file_path, 'in_incli_z_raw').read_text()
+        return [ self.get_incl_value(x), self.get_incl_value(y), self.get_incl_value(z) ]
     
     def get_incl_value(self, value):
         v = int(value)
